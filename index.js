160681df154506f9b014e504e16f3f9f4c6da29b
Caitlin Jordan <jordan.cait17@gmail.com>
2023-04-12 08:48:40 -0700

    formatted city search results to work in API url

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ form.addEventListener('submit', (event) => {
     event.preventDefault();
     console.log('event:', event);
     const searchResult = document.querySelector('#search').value
+    const citySplit = searchResult.split(' ')
+    const city = citySplit.join('_')
     fetch(`https://api.openbrewerydb.org/v1/breweries?by_city=${city}&per_page=100`)
     .then(response => response.json())
     .then(response => {
