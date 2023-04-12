aab3d63105bf2d504343d6357676fd683dd553f8
Caitlin Jordan <jordan.cait17@gmail.com>
2023-04-12 09:02:04 -0700

    added descriptions to js in comments

--- index.js
+++ index.js
@@ -16,25 +16,30 @@ form.addEventListener('submit', (event) => {
 })
 
 function showBrewery(brewery){
-    //adds cards that populate with brewery name
+    //add cards to populate name
     const breweryCardCollection = document.querySelector('#brewery-cards');
     const div = document.createElement('div');
     div.classList.add('card');
     div.setAttribute('id', brewery.name)
     const h1 = document.createElement('h1');
     h1.textContent = brewery.name;
+    //add brewery type so user can see what kind of brewery it is
     const type = document.createElement('h3');
     type.textContent = brewery.brewery_type
+    //add image to make cards more engaging & appealing
     const img = document.createElement('img');
     img.src = 'https://icons-for-free.com/iconfiles/png/512/beer-131982518583696551.png'
     img.classList.add('card-img')
+    //add address so user can easily find out where brewery is
     const street = document.createElement('h3');
     street.textContent = brewery.street
+    //add button to save brewery in list (also basis for event listener 'click')
     const btn = document.createElement('button');
     btn.setAttribute('type', 'button');
     btn.textContent = 'Visited'
 
-
-    div.append(h1, type, street, img, btn);
+    //append all elements to the div
+    div.append(h1, type, img, street, btn);
+    //append div to div in index.html
     breweryCardCollection.append(div);
 }
