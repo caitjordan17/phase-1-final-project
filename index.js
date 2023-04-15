409ca2d6f4f860764b120d2ed77f355d6382627c
Caitlin Jordan <jordan.cait17@gmail.com>
2023-04-15 14:38:21 -0700

    add h1 element to brewery saved list

--- index.js
+++ index.js
@@ -51,4 +51,6 @@ function addBreweryToList(breweryName){
     const favoriteBreweries = document.querySelector('#brewery-list')
     const div = document.createElement('div');
     div.classList.add('fav-card');
+    const h1 = document.createElement('h1');
+    h1.textContent = breweryName
 }
